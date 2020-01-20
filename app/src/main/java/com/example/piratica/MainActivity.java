b34a417aab47a54c7b12ed1d8039e211a82ef29b
Ashish Kumar singh <blr.ashu@gmail.com>
2020-01-20 23:08:13 +0000

    unable to fix SSID problem

--- app/src/main/java/com/example/piratica/MainActivity.java
+++ app/src/main/java/com/example/piratica/MainActivity.java
@@ -4,12 +4,16 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.SupplicantState;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.net.wifi.WifiInfo;
 import android.os.Bundle;
 import android.text.format.Formatter;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -34,7 +38,10 @@ public class MainActivity extends AppCompatActivity {
         if (wifiInfo == null) {
 
         } else {
-            ssid.setText("SSID: " + wifiInfo.getSSID());
+            if (wifiInfo.getSupplicantState() == SupplicantState.COMPLETED) {
+                ssid.setText("SSID: " + wifiInfo.getSSID());
+            }
+
 
             int ipAddress = wifiInfo.getIpAddress();
             String FormatedIpAddress2 = String.format("%d.%d.%d.%d",
